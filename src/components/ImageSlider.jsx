e46ec25a538bf824f1046288d3fab42aeb8d4965
minbr0ther <minbr0ther@naver.com>
2022-01-16 16:45:35 +0900

    feat: infinite sliding with transition

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,13 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Wrapper, Slider, ImageWrapper } from './styles/ImageSliderElements';
 
-const ImageSlider = ({
-  images = [],
-  autoPlay = true,
-  autoPlayTime = 4000,
-  ...props
-}) => {
+const ImageSlider = ({ images = [], autoPlayTime = 4000, ...props }) => {
   const [currentSlide, setCurrentSlide] = useState(1);
+  const [isClone, setIsClone] = useState(false);
 
   const nextSlide = (slideIndex = currentSlide + 1) => {
     const newSlideIndex = slideIndex >= images.length ? 0 : slideIndex;
@@ -15,9 +11,21 @@ const ImageSlider = ({
     setCurrentSlide(newSlideIndex);
   };
 
+  const onTransitionEnd = () => {
+    if (currentSlide === images.length - 1) {
+      setIsClone(true);
+      setCurrentSlide(1);
+      setTimeout(() => setIsClone(false), 100);
+    }
+    if (currentSlide === 0) {
+      setIsClone(true);
+      setCurrentSlide(images.length - 2);
+      setTimeout(() => setIsClone(false), 100);
+    }
+  };
+
   useEffect(() => {
     const timer = setTimeout(() => {
-      // 현재 이미지의 인덱스가 전체 이미지 수 보다 크면 첫번째로 돌아간다
       nextSlide();
     }, autoPlayTime);
 
@@ -31,6 +39,8 @@ const ImageSlider = ({
           key={index}
           currentIndex={index}
           currentSlide={currentSlide}
+          isClone={isClone}
+          onTransitionEnd={onTransitionEnd}
         >
           <Slider style={{ backgroundImage: `url(${image.url})` }}></Slider>
         </ImageWrapper>
