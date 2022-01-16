fbdb37033a19e01ef627cd78c2e0d5186e994146
minbr0ther <minbr0ther@naver.com>
2022-01-16 15:07:20 +0900

    feat: load image json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import NavigationBar from './components/GNB';
 import GlobalStyle from './components/styles/GlobalStyle';
+import images from './images/images.json';
 
 function App() {
+  const imagesWithBothEnds = () => {
+    const imageArray = images.data;
+    const firstClone = imageArray[0];
+    const lastClone = imageArray[imageArray.length - 1];
+
+    return [lastClone, ...imageArray, firstClone];
+  };
+
   return (
     <>
       <GlobalStyle />
       <NavigationBar />
+      {console.log(imagesWithBothEnds())}
     </>
   );
 }
